fdafc4829ce11655b7c62663bc0cfa041d2b6141
ved malpe <vedmalpe21@gmail.com>
2025-08-06 23:14:02 +0530

    Create  program1.cpp

---
+++ program1.cpp
@@ -0,0 +1,17 @@
+//find positive and negative number
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter number: ";
+    cin>>a;
+    cout<<a;
+    if(a>0)
+    {
+        cout<<" is a Positive Number";
+    }
+    else
+    cout<<" is a Negative Number";
+    
+}
